685b89329fe9298ae11c19fc751d5e11a01f55fb
altanner <8190834+altanner@users.noreply.github.com>
2021-10-04 16:39:36 +0100

    tidy

--- UTF16_TSV_to_UTF8_CSV.sh
+++ UTF16_TSV_to_UTF8_CSV.sh
@@ -5,12 +5,12 @@
 # Outputs a file prepended with "UTF8_"
 
 if [[ $# == 0 ]] ; then
-    echo "Please provide a file to convert."
-    exit 0
+    echo "Please provide a file to convert.";
+    exit 0;
 fi
 
-iconv -f UTF-16 -t UTF-8 $1 | sed 's/'$'\t''/,/g' > UTF8_$1
-echo "OK, converted $1 from UTF16 tab separated to UTF8 comma separated."
-echo "Output file name: UTF8_$1"
+iconv -f UTF-16 -t UTF-8 $1 | sed 's/'$'\t''/,/g' > UTF8_$1;
+echo "OK, converted $1 from UTF16 tab separated to UTF8 comma separated.";
+echo "Output file name: UTF8_$1";
 
 
