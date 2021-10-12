175da1c89859bd3c4f1829d2422d1252821de80f
altanner <8190834+altanner@users.noreply.github.com>
2021-10-12 16:28:04 +0100

    boots querier

--- boots_PG_querier.py
+++ boots_PG_querier.py
@@ -16,8 +16,8 @@ import psycopg2
 def args_setup():
 
     parser = argparse.ArgumentParser(
-        description="PostgreSQL DB Querier",
-        epilog="Example: python pg_querier.py -d database1 -t table1 --customer CUST001 --date 20180621 --spend")
+        description="PostgreSQL DB Querier: Boots transaction data",
+        epilog="Example: python pg_querier.py -d database1 -t table1 --customer 9874786793 --date 20180621 --spend")
     parser.add_argument(
         "--details", action="store_true",
         help="Provide DB and table information.")
@@ -29,25 +29,13 @@ def args_setup():
         help="The name of the table to query.")
     parser.add_argument(
         "--customer", action="store",
-        help="Customer code to query. Format: CUST0123456789")
+        help="Customer code to query. Format: 9874786793")
     parser.add_argument(
         "--product", action="store",
-        help="Product code to query. Format: PRD0123456")
-    parser.add_argument(
-        "--hour", action="store",
-        help="Shop hour to query (24 hour, 2 digits). Format: HH")
+        help="Product code to query. Format: 8199922")
     parser.add_argument(
         "--date", action="store",
         help="Shop date to query. Format: YYYYMMDD")
-    parser.add_argument(
-        "--week", action="store",
-        help="Shop week (of year) to query. Format: YYYYNN")
-    parser.add_argument(
-        "--weekday", action="store",
-        help="Shop weekday (1-7) to query. Format: N")
-    parser.add_argument(
-        "--basket", action="store",
-        help="Basket ID. Format: 123450123456789")
     parser.add_argument(
         "--count", action="store_true",
         help="Return total record counts.")
@@ -92,25 +80,6 @@ def all_records_from_product(
         print(e)
 
 
-def all_records_from_hour(
-    hour,
-    record_type,
-    table,
-    cursor,
-    connection):
-
-    sql = Template("""
-        SELECT $record_type FROM $table
-        WHERE SHOP_HOUR = '$hour';""")
-
-    try:
-        cursor.execute(sql.substitute(record_type=record_type, table=table, hour=hour))
-        result = cursor.fetchall()
-        output_type(record_type, result)
-    except Exception as e:
-        print(e)
-
-
 def all_records_from_date(
     date,
     record_type,
@@ -130,43 +99,6 @@ def all_records_from_date(
         print(e)
 
 
-def all_records_from_week(
-    week,
-    record_type,
-    table,
-    cursor,
-    connection):
-
-    sql = Template("""
-        SELECT $record_type FROM $table
-        WHERE SHOP_WEEK = '$week';""")
-
-    try:
-        cursor.execute(sql.substitute(record_type=record_type, table=table, week=week))
-        result = cursor.fetchall()
-        output_type(record_type, result)
-    except Exception as e:
-        print(e)
-
-
-def all_records_from_weekday(
-    weekday,
-    record_type,
-    table,
-    cursor,
-    connection):
-
-    sql = Template("""
-        SELECT $record_type FROM $table
-        WHERE SHOP_WEEKDAY = '$weekday';""")
-
-    try:
-        cursor.execute(sql.substitute(record_type=record_type, table=table, weekday=weekday))
-        result = cursor.fetchall()
-        output_type(record_type, result)
-    except Exception as e:
-        print(e)
-
 #~ CUSTOMER queries =========================
 def customer_records_all(
     customer,
@@ -177,7 +109,7 @@ def customer_records_all(
 
     sql = Template("""
         SELECT $record_type FROM $table
-        WHERE CUST_CODE = '$customer';""")
+        WHERE ID = '$customer';""")
 
     try:
         cursor.execute(sql.substitute(record_type=record_type, table=table, customer=customer))
@@ -320,26 +252,6 @@ def customer_records_for_product_from_weekday(
         print(e)
 
 
-#~ BASKET QUERIES ==========================
-def basket_all_records(
-    basket,
-    record_type,
-    table,
-    cursor,
-    connection):
-
-    sql = Template("""
-        SELECT $record_type FROM $table
-        WHERE BASKET_ID = '$basket';""")
-
-    try:
-        cursor.execute(sql.substitute(record_type=record_type, table=table, basket=basket))
-        result = cursor.fetchall()
-        output_type(record_type, result)
-    except Exception as e:
-        print(e)
-
-
 #~ GENERAL STATUS QUERY =====================
 def db_details(
     db,
@@ -382,6 +294,43 @@ def db_details(
         print(f"Customers:   {cust_count[0][0]}")
         print(f"Baskets:     {basket_count[0][0]}")
         print(f"Shop dates:  {date_count[0][0]}")
+    except Exception as e:
+        print(e)
+
+def db_scrape_details(
+    db,
+    table,
+    cursor,
+    connection):
+
+    """
+    Return some information about the current state of Postgres.
+    """
+
+    sql_column_count = Template("""
+        SELECT COUNT(*)
+        FROM information_schema.columns
+        WHERE table_name='$table';""")
+    sql_record_count = Template("""
+        SELECT COUNT(*)
+        FROM $table;""")
+    sql_product_count = Template("""
+        SELECT COUNT (DISTINCT PRODUCTID) FROM $table;""")
+
+    try:
+        cursor.execute(sql_column_count.substitute(table=table))
+        column_count = cursor.fetchall()
+        cursor.execute(sql_record_count.substitute(table=table))
+        record_count = cursor.fetchall()
+        cursor.execute(sql_product_count.substitute(table=table))
+        product_count = cursor.fetchall()
+        print(f"DB connection details:\n", connection.get_dsn_parameters())
+        print(f"\n{table} details:Columns:     {column_count[0][0]}")
+        print(f"Records:     {record_count[0][0]}")
+        print(f"Products:    {product_count[0][0]}")
+        if record_count[0][0] != product_count[0][0]:
+            discrepancy = record_count[0][0] - product_count[0][0]
+            print(f"\n!!! Note: record and product counts are not equal. \nThis is probably due to {discrepancy} products having null id codes.")
     except Exception as e:
         print(e)
 
