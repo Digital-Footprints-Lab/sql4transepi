222c560c63db47f8f4d79788ea9935c4e6a1c7d0
altanner <8190834+altanner@users.noreply.github.com>
2021-10-28 16:21:55 +0100

    added basket_id field, sorry still a bit repetitive

--- tesco_card_JSON2CSV.py
+++ tesco_card_JSON2CSV.py
@@ -23,38 +23,21 @@ def args_setup():
 parser, args = args_setup()
 
 
-def generate_hash_id(json_file):
+def generate_hash_field(hash_input):
 
     """
-    Tesco card JSONs do not have a unique and persistent user identifier:
-    the card number is obfuscated, and there are no other
-    identifiers to use. This function generates a hex hash,
-    using the transaction storeId and timestamp for the first even transaction
-    as the hashable. This should be unique and immutable, unless Tesco
-    change the format of their JSONs.
-
-    Args:       A JSON file of Tesco Clubcard user details and transactions
+    Args:       Some text to be hashed
 
     Returns:    A unique 16 char hex identifier.
     """
 
-    with open(json_file) as json_infile:
-        data = json.load(json_infile)
+    _hash = hashlib.sha1(str(hash_input).encode())
+    _hash_hex = _hash.hexdigest()[0:16]
 
-    #~ get the storeid and timestamp of their first even transaction
-    store_id = data["Purchase"][0][0]["storeId"]
-    timestamp = data["Purchase"][0][0]["timeStamp"]
-    hash_input = store_id + timestamp
-    #~ hash it
-    cust_hash = hashlib.sha1(str(hash_input).encode())
-    cust_hex = cust_hash.hexdigest()[0:16]
-
-    print(f"\nAssigning hash as customer identifier: {cust_hex}")
-
-    return cust_hex
+    return _hash_hex
 
 
-def json_items_to_csv_file(json_file, customer_id):
+def json_items_to_csv_file(json_file):
 
     """
     Does the JSON nest-diving to create a single row per item.
@@ -74,21 +57,33 @@ def json_items_to_csv_file(json_file, customer_id):
     with open(json_file) as json_infile:
         data = json.load(json_infile)
 
+    #~ Tesco card JSONs do not have a unique and persistent user identifier:
+    #~ Here we assign one by hashing their 1st transaction metadata,
+    #~ which should be unique and immutable
+    transaction1_store_id = data["Purchase"][0][0]["storeId"]
+    transaction1_timestamp = data["Purchase"][0][0]["timeStamp"]
+    customerId = generate_hash_field(transaction1_store_id + transaction1_timestamp)
+    print(f"\nAssigning hash as customer identifier: {customerId}")
+
     #~ The "Purchase" nest is a >>list<< of transactions
     for transaction in data["Purchase"][0]:
         storeId = transaction["storeId"]
         timeStamp = transaction["timeStamp"]
+        #~ generate basket identifier hash
+        basketId = generate_hash_field(storeId + timeStamp)
         #~ The "product" nest is a >>dict<< of items in the transaction
         for item in transaction["product"]:
             item = OrderedDict(item)
-            #~ add storeId + prepend
             item["storeId"] = storeId
             item.move_to_end("storeId", last=False)
             #~ add timestamp + prepend
             item["timeStamp"] = timeStamp
             item.move_to_end("timeStamp", last=False)
+            #~ add basketid + prepend
+            item["basketId"] = basketId
+            item.move_to_end("basketId", last=False)
             #~ add our hash-generated customer ID + prepend
-            item["customerId"] = customer_id
+            item["customerId"] = customerId
             item.move_to_end("customerId", last=False)
             #~ add the whole lot to our item list
             items.append(item)
@@ -112,9 +107,7 @@ def json_items_to_csv_file(json_file, customer_id):
 
 def main():
 
-    customer_id = generate_hash_id(args.input.name)
-
-    json_items_to_csv_file(args.input.name, customer_id)
+    json_items_to_csv_file(args.input.name)
 
 
 if __name__ == "__main__":
